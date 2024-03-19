65bed86a47e8667f9cf4c5c0f0f195264ea893b2
Igor Gomes <igorgomes0903@gmail.com>
2024-03-19 17:04:52 -0300

    Inserção de CRUD Marca, categoria e ajuste nas classes Cidade

---
+++ Backend/src/main/java/com/dev/Backend/repository/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.dev.Backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dev.Backend.entity.Cidade;
+
+public interface CidadeRepository extends JpaRepository<Cidade, Long>{
+	
+	
+
+}
